5ab5ae64ee89d7a4e78d234aed9bcb0e385cdb48
Himanshu Sharma <himanshu0719.cse19@chitkara.edu.in>
2022-01-20 15:48:17 +0530

    creating controller

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ const express=require('express');
 
 const router = express.Router();
 
+const homeController= require('../controller/home_controller');
+
 
 console.log('router loaded')  //to check if the router is loaded
+router.get('/',homeController.home)
 module.exports=router;

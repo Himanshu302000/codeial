62cfa08d429620d2bd6cb015071c4a3969ca722f
Himanshu Sharma <himanshu0719.cse19@chitkara.edu.in>
2022-01-24 15:53:05 +0530

    Added PArtials

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -1,5 +1,5 @@
 module.exports.profile=function(req,res){
-    return res.end('<h1>user profile</h1>');
+    return res.render('user');
 }
 module.exports.posts=function(req,res){
     return res.end('<h1>Posts Section</h1>')
